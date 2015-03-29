d46047f4edf2d8d33669c509c5b6f167b649f63e
stefaniab <stefania13@ru.is>
2015-03-29 17:24:53 +0000

    newSolve

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,20 @@ public class Main
 {
 	static public void main(String[] args)
 	{
-/*		for (int i = 0; i < 3 ; i++)
+		for (int i = 0; i < 10 ; i++)
 		{
-			VarBoard b = new VarBoard(Generator.get(4, i), 4);
+			VarBoard b = new VarBoard(Generator.get(3, i), 3);
 			Stopwatch timer = new Stopwatch();
 			b.solve();
 			System.out.println("Brute force: \t" + timer.elapsedTime() + " time: \t" + b.counter);
-			b = new VarBoard(Generator.get(4, i), 4);
+			b = new VarBoard(Generator.get(3, i), 3);
 			b.fancyPrint();
 			b.counter = 0;
 			timer = new Stopwatch();
 			b.initialRestrict();
 			b.newSolve();
 			System.out.println("Queue: \t" + timer.elapsedTime() + " time: \t" + b.counter);
-		}*/
+		}
 
 		/*int[][] board4 = Generator.getBoard4(); 
 		Board b4 = new Board(board4, 4);
