a02a71ed9fd0cdb0dc45c89c7242d048138b4c24
stefaniab <stefania13@ru.is>
2015-03-29 15:43:22 +0000

    Main

--- Main.java
+++ Main.java
@@ -1,49 +1,25 @@
 package Sudoku;
 
+import java.util.Collection;
+import java.util.*;
+
 public class Main
 {
 	static public void main(String[] args)
 	{
-		/*int[][] board4 = Generator.getBoard4(); 
-		Board b4 = new Board(board4, 4);
-		
-		b4.fancyPrint();
-		
-		Stopwatch timer2 = new Stopwatch();
-		b4.solve();
-<<<<<<< HEAD
-		System.out.println(timer2.elapsedTime());
-		b4.fancyPrint();*/
-
-		//int[][] board = Generator.getHardBoard(); 
-		//Board b = new Board(board, 3);
-		//b.fancyPrint();
-		//Stopwatch timer = new Stopwatch();
-		//b.solve();
-		//System.out.println(timer.elapsedTime());
-		//Board b = new Board(Generator.getBoard4(), 4);
-		//b.fancyPrint();
-		//b.solve();
-		//b.fancyPrint();
-		
-		VarBoard v = new VarBoard(Generator.getBoard4(), 4);
-		v.fancyPrint();
-		Stopwatch timer = new Stopwatch();
-		v.solve();
-		System.out.println(timer.elapsedTime());
-		/*System.out.println(timer2.elapsedTime());*/
-		
-
-		/*Variable[][] board = Generator.getHardBoard(); 
-		Board b = new Board(board, 3);
+		int[][] smallBoard = {{0, 1, 3, 0}, {2, 0, 0, 0}, {0, 0, 0, 3}, {0, 2, 1, 0}};
+		VarBoard b = new VarBoard(Generator.get(3, 0), 3);
 		b.fancyPrint();
 		Stopwatch timer = new Stopwatch();
 		b.solve();
-		System.out.println(timer.elapsedTime());*/
+		System.out.println("Brute force: \t" + timer.elapsedTime() + " counter: \t" + b.counter);
+		b.counter = 0;
+		b = new VarBoard(Generator.get(3, 0), 3);
+		timer = new Stopwatch();
+		b.initialRestrict();
 		
-		Generator gen = new Generator(3);
-		Board randomBoard = gen.getRandomBoard();
-		randomBoard.fancyPrint();
+		b.newSolve();
+		System.out.println("Queue solve: \t" + timer.elapsedTime() + " counter: \t" + b.counter);
 		
 	}
 }
