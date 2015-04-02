2dc964be58670d01a2c0776d3dc7af11e7be2fad
Arnar Freyr Bjarnason <arnarb12@ru.is>
2015-04-02 12:41:39 +0000

    hardcoded boards are now in HardBoard.java, generator mostly clean. Builds and runs through

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main
 	{
 		int i = 6;
 		
-		VarBoard b = new VarBoard(Generator.get(4, i), 4);
+		VarBoard b = new VarBoard(Generator.getHardBoard(4, i), 4);
 		b.fancyPrint();
 		Stopwatch timer = new Stopwatch();
 		b.solve();
 		System.out.println("Solved: \t time: " + timer.elapsedTime() + "\t states: " + b.counter);
 		
-		b = new VarBoard(Generator.get(4, i), 4);
+		b = new VarBoard(Generator.getHardBoard(4, i), 4);
 		b.counter = 0;
 		timer = new Stopwatch();
 		b.initialRestrict();
